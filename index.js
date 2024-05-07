9fc92e463d1629c02c1b281a3ba7618c36522ebf
samarali <engsamar105@gmail.com>
2024-05-07 03:30:20 +0300

    Room with CRUD Operation l

--- index.js
+++ index.js
@@ -4,41 +4,42 @@ const cors = require("cors");
 
 const express = require("express");
 const app = express();
-app.use(express.json())
 
 const mainRouter = express.Router();
-//reservation Satus
-const reservationSatusRouter = require('./routes/reservation-status.router')
 
+// Importing repositories
 const ReservationSatusRepository = require('./repositories/reservation-status.repository')
-
-const ReservationSatusController = require('./controllers/reservation-status.controller')
-
-//Room Type
-const roomTypeRouter = require('./routes/room-type.router')
-
 const RoomTypeRepository = require('./repositories/room-type.repository')
+const RoomRepository = require("./repositories/room.repository");
 
+// Importing controllers
 const RoomTypeController = require('./controllers/room-type.controller')
+const ReservationSatusController = require('./controllers/reservation-status.controller')
+const RoomController = require("./controllers/room.controller");
 
+// Importing routers
+const reservationSatusRouter = require('./routes/reservation-status.router')
+const roomTypeRouter = require('./routes/room-type.router');
+const roomRouter = require("./routes/room.router");
 
-//reservation Satus
-const reservationSatusRepository = new ReservationSatusRepository()
-
-const reservationSatusController = new ReservationSatusController(reservationSatusRepository)
-//Room Type
-
-const roomTypeRepository = new RoomTypeRepository()
-
-const roomTypeController = new RoomTypeController(roomTypeRepository)
-
+app.use(express.json())
 app.use(cors());
-
 app.use('/api/v1', mainRouter);
 
-mainRouter.use('/reservation-status', reservationSatusRouter(reservationSatusController))
+// Creating instances of repositories
+const reservationSatusRepository = new ReservationSatusRepository()
+const roomTypeRepository = new RoomTypeRepository()
+const roomRepository=new RoomRepository()
 
+// Creating instances of controllers
+const reservationSatusController = new ReservationSatusController(reservationSatusRepository)
+const roomTypeController = new RoomTypeController(roomTypeRepository)
+const roomController = new RoomController(roomRepository)
+
+// routers with controllers
+mainRouter.use('/reservation-status', reservationSatusRouter(reservationSatusController))
 mainRouter.use('/room-type', roomTypeRouter(roomTypeController))
+mainRouter.use('/rooms',roomRouter(roomController))
 
 app.listen(3000, () => {
     console.log(`listening on port ${3000} ...`);
---
+++ repositories/room.repository.js
@@ -0,0 +1,27 @@
+const Room = require("../models/room.model")
+
+class roomRepository{
+    async getAllRooms(){
+        //populate('hotelId)
+        return await Room.find().populate('roomTypeId');
+    }
+    async getRoomById (id){
+        //populate('hotelId)
+        return await Room.findOne(id).populate('roomTypeId');
+    
+    }
+    
+    async addRoom(req){
+        return await Room.create(req);
+    
+    }
+    async  editRoom(id,req){
+        return await Room.updateOne(id,req);
+    
+    }
+    async deleteRoom (id){
+        return  await Room.deleteOne(id)
+    }
+}
+
+module.exports = roomRepository;
