428293fa4c5e18bc241fdfccab464a44123fa10b
esraaali138 <esraaali1999138@gmail.com>
2024-05-09 02:05:08 -0700

    add amenty and hotel endpoints

--- db/index.js
+++ db/index.js
@@ -1,8 +1,8 @@
 const mongoose = require ('mongoose');
 
-const dburi = process.env.DB_URI ;
-const dbConnection = mongoose.connect(process.env.DB_URI).
-then((data)=>console.log(data.connection.host))
+const dburi = process.env.DB_URL ;
+const dbConnection = mongoose.connect(dburi).
+then((data)=>console.log("Successful connection to Mongodb"))
 .catch((err)=>console.log(err))
 module.exports = dbConnection
 
--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 require("dotenv").config();
 require("./db");
-var cors = require("cors");
-
 const express = require("express");
+const bodyParser = require('body-parser');
+
 const app = express();
 
+app.use(bodyParser.json());
+const cors = require('cors')
 app.use(cors());
 
+// Router
+const hotelRouter = require('./routes/hotel.router');
+const amentyRouter = require('./routes/amenty.router');
+///
+const HotelController = require('./controllers/hotel.controller');
+const HotelRepository = require('./repository/hotel.repository');
+const AmentyController = require('./controllers/amenty.controller');
+const AmentyRepository = require('./repository/amenty.repository');
+
+//get insatance of HotelController
+const hotelRepository = new HotelRepository();
+//get insatance of HotelRepository
+const hotelController = new HotelController(hotelRepository);
+//get insatance of AmentyRepository 
+const  amentyRepository = new AmentyRepository();
+//get insatance of AmentyController
+const  amentyController = new AmentyController(amentyRepository)
+
+const mainRouter = express.Router();
+mainRouter.use('/hotels', hotelRouter(hotelController));
+mainRouter.use('/amenties' ,amentyRouter(amentyController) )
+app.use('/api/v1', mainRouter);
+
 app.listen(3000, () => {
-    console.log(`listening on port ${3000} ...`);
-  });
+    console.log(`Listening on port ${3000} ...`);
+});
