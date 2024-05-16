9d6684722c1723855cbcdd1cfb6e187b267f7e61
esraaali138 <esraaali1999138@gmail.com>
2024-05-15 23:16:12 -0700

    add

--- routes/amenty.router.js
+++ routes/amenty.router.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const router = express.Router();
-const { protect, restrictTo } = require('../middleware/auth');
-const {uploadMultiple} = require("../middleware/multer");
-const {uploadImage} = require("../middleware/firebase")
+const { protect, restrictTo } = require("../middleware/auth");
+const { uploadMultiple } = require("../middleware/multer");
+const { uploadImage } = require("../middleware/firebase");
 const { deleteImages } = require("../middleware/firebase");
-
 
 const amentyRouter = (amentyController) => {
   router.get("/", async (req, res) => {
     try {
-      const getAmenties = await amentyController.getAllAmenties();
-      res.send(getAmenties);
+      const amenties = await amentyController.getAllAmenties();
+      res.send(amenties);
     } catch (error) {
-      res.status(500).json({ message: "Server Error" + error.message });
+      res.status(500).json({ message: error.message });
     }
   });
 
@@ -25,18 +24,25 @@ const amentyRouter = (amentyController) => {
       }
       res.send(amenty);
     } catch (error) {
-      res.status(500).json({ message: "Server Error: "+ error.message  });
+      res.status(500).json({ message: error.message });
     }
   });
 
-  router.post("/", protect, restrictTo("admin"),uploadMultiple , uploadImage,  async (req, res) => {
-    try {
-      await amentyController.addAmenty(req.body);
-      res.status(201).send("the amenty added successfully");
-    } catch (error) {
-      res.status(500).json({message: "Server Error: "+ error.message  });
+  router.post(
+    "/",
+    protect,
+    restrictTo("admin"),
+    uploadMultiple,
+    uploadImage,
+    async (req, res) => {
+      try {
+        await amentyController.addAmenty(req.body);
+        res.status(201).send("the amenty added successfully");
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
     }
-  });
+  );
 
   router.delete("/:id", protect, restrictTo("admin"), async (req, res) => {
     try {
@@ -46,34 +52,40 @@ const amentyRouter = (amentyController) => {
         return;
       }
       if (req.body.images) {
-        await deleteImages(room.images)
+        await deleteImages(room.images);
       }
       await amentyController.deleteAmenty(req.params.id);
       res.status(200).send("The amenty deleted successfully");
     } catch (error) {
-      res.status(500).json({ message: "Server Error" + error.message  });
+      res.status(500).json({ message: error.message });
     }
   });
 
-  router.patch("/:id", protect, restrictTo("admin"),uploadMultiple , uploadImage,  async (req, res) => {
-    try {
-      const amenty = await amentyController.getAmentyById(req.params.id);
-      if (!amenty) {
-        res.status(404).send("this amenty does not exist");
-        return;
+  router.patch(
+    "/:id",
+    protect,
+    restrictTo("admin"),
+    uploadMultiple,
+    uploadImage,
+    async (req, res) => {
+      try {
+        const amenty = await amentyController.getAmentyById(req.params.id);
+        if (!amenty) {
+          res.status(404).send("this amenty does not exist");
+          return;
+        }
+        if (req.body.images) {
+          await deleteImages(room.images);
+        }
+        await amentyController.editAmenty(req.params.id, req.body);
+        res.status(200).send("The amenty updated successfully");
+      } catch (error) {
+        res.status(500).json({ message: error.message });
       }
-      if (req.body.images) {
-        await deleteImages(room.images)
-      }
-      await amentyController.editAmenty(req.params.id, req.body);
-      res.status(200).send("The amenty updated successfully");
-    } catch (error) {
-      res.status(500).json({ message: "Server Error" + error.message });
     }
-  });
+  );
 
   return router;
 };
 
 module.exports = amentyRouter;
-
--- routes/hotel.router.js
+++ routes/hotel.router.js
@@ -1,24 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const { protect, restrictTo } = require('../middleware/auth')
-const {uploadMultiple} = require("../middleware/multer");
-const {uploadImage} = require("../middleware/firebase");
+const { protect, restrictTo } = require("../middleware/auth");
+const { uploadMultiple } = require("../middleware/multer");
+const { uploadImage } = require("../middleware/firebase");
 const { deleteImages } = require("../middleware/firebase");
 
-
 const hotelRouter = (hotelController, roomController) => {
-//-----------------------find room with hotleId----------------------
+  //-----------------------find room with hotleId----------------------
   router.use("/:hotelId/rooms", roomController.getAllRooms);
   //-------------------------------------------------//
   router.get("/", async (req, res) => {
     try {
-      const Allhotels = await hotelController.getAllHotels();
-      res.send(Allhotels);
+      const hotels = await hotelController.getAllHotels();
+      res.send(hotels);
     } catch (error) {
-      res.status(500).json({ message: "Server Error: "+ error.message });
+      res.status(500).json({ message: "Server Error: " + error.message });
     }
   });
-  
+
   router.get("/:id", protect, async (req, res) => {
     try {
       const hotel = await hotelController.getHotelById(req.params.id);
@@ -28,18 +27,25 @@ const hotelRouter = (hotelController, roomController) => {
       }
       res.send(hotel);
     } catch (error) {
-      res.status(500).json({ message: "Server Error: "+ error.message });
+      res.status(500).json({ message: "Server Error: " + error.message });
     }
   });
-  
-  router.post("/", protect, restrictTo("admin"),uploadMultiple , uploadImage, async (req, res) => {
-    try {
-      await hotelController.addHotel(req.body);
-      res.status(201).send("the hotel added successfully");
-    } catch (error) {
-      res.status(500).json({ message: "Server Error: "+ error.message });
+
+  router.post(
+    "/",
+    protect,
+    restrictTo("admin"),
+    uploadMultiple,
+    uploadImage,
+    async (req, res) => {
+      try {
+        await hotelController.addHotel(req.body);
+        res.status(201).send("the hotel added successfully");
+      } catch (error) {
+        res.status(500).json({ message: "Server Error: " + error.message });
+      }
     }
-  });
+  );
 
   router.delete("/:id", protect, restrictTo("admin"), async (req, res) => {
     try {
@@ -49,31 +55,38 @@ const hotelRouter = (hotelController, roomController) => {
         return;
       }
       if (req.body.images) {
-        await deleteImages(room.images)
+        await deleteImages(room.images);
       }
       await hotelController.deleteHotel(req.params.id);
       res.status(200).send("The hotel was deleted successfully");
     } catch (error) {
-      res.status(500).json({ message: "Server Error: "+ error.message });
+      res.status(500).json({ message: "Server Error: " + error.message });
     }
   });
 
-  router.patch("/:id", protect, restrictTo("admin"),uploadMultiple , uploadImage, async (req, res) => {
-    try {
-      const hotel = await hotelController.getHotelById(req.params.id);
-      if (!hotel) {
-        res.status(404).send("this hotel does not exist");
-        return;
+  router.patch(
+    "/:id",
+    protect,
+    restrictTo("admin"),
+    uploadMultiple,
+    uploadImage,
+    async (req, res) => {
+      try {
+        const hotel = await hotelController.getHotelById(req.params.id);
+        if (!hotel) {
+          res.status(404).send("this hotel does not exist");
+          return;
+        }
+        if (req.body.images) {
+          await deleteImages(room.images);
+        }
+        await hotelController.editHotel(req.params.id, req.body);
+        res.status(200).send("This hotel updated successfully");
+      } catch (error) {
+        res.status(500).json({ message: "Server Error: " + error.message });
       }
-      if (req.body.images) {
-        await deleteImages(room.images)
-      }
-      await hotelController.editHotel(req.params.id, req.body);
-      res.status(200).send("This hotel updated successfully");
-    } catch (error) {
-      res.status(500).json({ message: "Server Error: "+ error.message });
     }
-  });
+  );
 
   return router;
 };
