befb1bcaf3d230bfe4a0416ddaa895381625d41a
samarali <engsamar105@gmail.com>
2024-06-13 14:45:36 +0300

    Edit subscriptionAdvantage and roomType

--- routes/roomType.js
+++ routes/roomType.js
@@ -1,88 +1,100 @@
-const express = require('express')
-const { protect, restrictTo } = require('../middlewares/auth')
+const express = require("express");
+const { protect, restrictTo } = require("../middlewares/auth");
 const {
   validateNewRoomType,
   validateUpdateRoomType,
-} = require('../validations/roomType')
-const BadRequestError = require('../handleErrors/badRequestError')
-const router = express.Router()
+} = require("../validations/roomType");
+const BadRequestError = require("../handleErrors/badRequestError");
+const router = express.Router();
 
 const roomTypeRouter = (roomTypeController) => {
-  router.get('/', async (req, res) => {
+  router.get("/", async (req, res) => {
     try {
-      const roomsType = await roomTypeController.getAllRoomsType()
+      const roomsType = await roomTypeController.getAllRoomsType();
       res.status(200).json({
-        status: 'success',
+        status: "success",
         data: roomsType,
-      })
+      });
     } catch (error) {
-      res.status(error.statusCode || 500).json({ message: error.message })
+      res.status(error.statusCode || 500).json({ message: error.message });
     }
-  })
-  router.post('/', protect, restrictTo('admin'), async (req, res) => {
+  });
+  router.post("/", protect, restrictTo("admin"), async (req, res) => {
     try {
-      const { error } = validateNewRoomType(req.body)
+      const { error } = validateNewRoomType(req.body);
       if (error) {
-        throw new BadRequestError(error.message)
+        throw new BadRequestError(error.message);
       }
-      const roomType = await roomTypeController.addRoomType({
-        ...req.body,
-      })
+      const roomType = await roomTypeController.addRoomType({ ...req.body });
       res.status(201).json({
-        status: 'success',
-        message: ' Rome Type added successfully',
+        status: "success",
+        message: " Rome Type added successfully",
         roomType: roomType,
-      })
+      });
     } catch (error) {
-      res.status(error.statusCode || 500).json({ message: error.message })
+      res.status(error.statusCode || 500).json({ message: error.message });
     }
-  })
-  router.patch('/:id', protect, restrictTo('admin'), async (req, res) => {
+  });
+  router.patch("/:id", protect, restrictTo("admin"), async (req, res) => {
     try {
-      const { id } = req.params
-      const room = await roomTypeController.getRoomTypeById({ _id: id })
-      const { error } = validateUpdateRoomType(req.body)
+      const { id } = req.params;
+      const room = await roomTypeController.getRoomTypeById({ _id: id });
+      const { error } = validateUpdateRoomType(req.body);
       if (error) {
-        throw new BadRequestError(error.message)
+        throw new BadRequestError(error.message);
       }
       const updateRoomType = await roomTypeController.editRoomType(
         { _id: id },
-        { ...req.body },
-      )
+        { ...req.body }
+      );
 
       const findNewRomeType = await roomTypeController.getRoomTypeById({
         _id: id,
-      })
+      });
       res.status(200).json({
-        status: 'success',
-        message: ' Room Type updated successfully',
+        status: "success",
+        message: " Room Type updated successfully",
         roomType: findNewRomeType,
-      })
+      });
     } catch (error) {
-      res.status(error.statusCode || 500).json({ message: error.message })
+      res.status(error.statusCode || 500).json({ message: error.message });
     }
-  })
-  router.delete('/:id', protect, restrictTo('admin'), async (req, res) => {
+  });
+  router.delete("/:id", protect, restrictTo("admin"), async (req, res) => {
     try {
-      const { id } = req.params
-      const room = await roomTypeController.getRoomTypeById({ _id: id })
+      const { id } = req.params;
+      const room = await roomTypeController.getRoomTypeById({ _id: id });
 
       if (!room) {
-        res.status(404).json('Room Type not found')
-        return
+        res.status(404).json("Room Type not found");
+        return;
       }
-      await roomTypeController.deleteRoomType({ _id: id })
+      await roomTypeController.deleteRoomType({ _id: id });
 
       res.status(200).json({
-        status: 'success',
-        message: ' Room Type deleted successfully',
-      })
+        status: "success",
+        message: " Room Type deleted successfully",
+      });
     } catch (error) {
-      res.status(error.statusCode || 500).json({ message: error.message })
+      res.status(error.statusCode || 500).json({ message: error.message });
     }
-  })
+  });
 
-  return router
-}
+  router.get("/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const room = await roomTypeController.getRoomTypeById({ _id: id });
+      res.status(200).json({
+        status: "success",
+        message: "Room Type founded",
+        data: room,
+      });
+    } catch (error) {
+      res.status(error.statusCode || 500).json({ message: error.message });
+    }
+  });
 
-module.exports = roomTypeRouter
+  return router;
+};
+
+module.exports = roomTypeRouter;
