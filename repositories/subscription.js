319e5e797932ffc68ce16dc424e1363ba840a7f5
samarali <engsamar105@gmail.com>
2024-06-19 18:56:54 +0300

    Edit subscription

--- repositories/subscription.js
+++ repositories/subscription.js
@@ -1,5 +1,6 @@
 const NotFoundError = require("../handleErrors/notFoundError");
 const Subscription = require("../models/subscription");
+const User = require("../models/user");
 
 class SubscriptionRepository {
   async getAllSubscriptions(skip, limit) {
@@ -21,11 +22,17 @@ class SubscriptionRepository {
     if (!subscription) {
       throw new NotFoundError("The Subscription with this ID was not found");
     }
+
     return subscription;
   }
 
-  async addSubscription(req) {
-    return await Subscription.create(req);
+  async addSubscription(req, userId) {
+    console.log(userId);
+    // const user = await User.findById(userId._id);
+    const subscription = await Subscription.create(req);
+    userId.subscriptionId = subscription._id;
+    await userId.save();
+    return userId;
   }
 
   async editSubscription(id, req) {
